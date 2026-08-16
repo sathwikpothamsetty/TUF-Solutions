378ff3aede0a2a35e8caa0c7d154952df46c8e97
sathwikpothamsetty <153756662+sathwikpothamsetty@users.noreply.github.com>
2026-08-16 11:06:12 +0530

    Sync Implement Min Stack [DSA] - TUFHub

--- DSA/Stack-Queue/implement-min-stack/solution.java
+++ DSA/Stack-Queue/implement-min-stack/solution.java
@@ -1,52 +1,49 @@
 class MinStack {
-        private Stack<Integer> st;
-        private int mini;
-      public MinStack() {
-        st=new Stack<>();   
+    private Stack<Long> st;
+    private long mini;
+
+    public MinStack() {
+        st = new Stack<>();
     }
 
     public void push(int val) {
-        if(st.isEmpty())
-        {
-            mini=val;
-            st.push(val);
-            return ;
+        if (st.isEmpty()) {
+            mini = val;
+            st.push((long) val);
+            return;
         }
-        if(val > mini)
-        {
-            st.push(val);
-        }else
-        {
-            st.push(2*val-mini);
 
-            mini=val;
+        if (val > mini) {
+            st.push((long) val);
+        } else {
+            st.push(2L * val - mini);
+            mini = val;
         }
-   
     }
 
     public void pop() {
-        if(st.isEmpty()) return;
-        
-        int x=st.pop();
-        if(x<mini)
-        {
-            mini=2*mini-x;
+        if (st.isEmpty()) return;
+
+        long x = st.pop();
+
+        if (x < mini) {
+            mini = 2L * mini - x;
         }
     }
 
     public int top() {
-        if(st.isEmpty()) return -1;
+        if (st.isEmpty()) return -1;
 
-        int x=st.peek();
+        long x = st.peek();
 
-        if(mini<x) return x;
+        if (x < mini) {
+            return (int) mini;
+        }
 
-        return mini;
-    
+        return (int) x;
     }
 
     public int getMin() {
-        return mini;
-        
+        return (int) mini;
     }
 }
